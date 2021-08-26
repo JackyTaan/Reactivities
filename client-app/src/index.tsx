2355537b09733dbad7d00f468d3a54d29f0f7fd2
Tan Lam Ngan <ngantl@sendo.vn>
2021-08-26 16:43:24 +0700

    End of section 7

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css'
+import { store, StoreContext } from './app/stores/store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StoreContext.Provider value={store}>
+      <App />
+  </StoreContext.Provider>, 
   document.getElementById('root')
 );
 
